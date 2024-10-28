eaf3cbdaf1000286429d81ef60691a0cb962a108
SahithiKunapaneni <125670050+SahithiKunapaneni@users.noreply.github.com>
2024-10-28 16:29:34 +0530

    Add files via upload

---
+++ fer_hibernate/src/main/java/com/rs/fer/bean/Address.java
@@ -0,0 +1,80 @@
+package com.rs.fer.bean;
+
+public class Address {
+	private int id;
+	private String lineOne;
+	private String lineTwo;
+	private String city;
+	private String state;
+	private String pinCode;
+	private String country;
+	 private User user;
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	
+	public String getLineOne() {
+		return lineOne;
+	}
+
+	public void setLineOne(String lineOne) {
+		this.lineOne = lineOne;
+	}
+
+	public String getLineTwo() {
+		return lineTwo;
+	}
+
+	public void setLineTwo(String lineTwo) {
+		this.lineTwo = lineTwo;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getPinCode() {
+		return pinCode;
+	}
+
+	public void setPinCode(String pinCode) {
+		this.pinCode = pinCode;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	
+
+}
